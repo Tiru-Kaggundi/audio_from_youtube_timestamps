d56c11d7e6825e8b024e908d0499f7b2803c6c6e
Tirumala Venkatesh Kaggundi <97689901+Tiru-Kaggundi@users.noreply.github.com>
2025-02-10 21:34:16 +0530

    ..
    ..

--- audio_extractor_1.py
+++ audio_extractor_1.py
@@ -1,6 +1,9 @@
 import os
 from pytube import YouTube
 from moviepy.editor import AudioFileClip
+import ssl
+import certifi
+ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
 
 
 def mmss_to_seconds(ts):
